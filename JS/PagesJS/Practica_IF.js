c8656d88fea1574727d509b8399dc25db2b5fafe
vlammp <mmorap@vlacademy.com>
2025-02-03 20:44:43 -0600

    Ejercicio logica con IF

--- JS/PagesJS/Practica_IF.js
+++ JS/PagesJS/Practica_IF.js
@@ -1,1 +1,13 @@
 console.log(`Funcionando Archivo: Practica_IF`);
+
+
+//Caso 1
+function verificarEdad() {
+
+    let edad = document.getElementById('edad').value;//gi
+    let pais = document.getElementById('pais').value;
+
+    console.log(edad)
+    console.log(pais)
+    
+}
