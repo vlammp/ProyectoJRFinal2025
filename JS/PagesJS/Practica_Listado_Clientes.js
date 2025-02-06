4eef88014d914c2a531a94038907013809c50553
vlammp <mmorap@vlacademy.com>
2025-02-05 21:23:30 -0600

    Listado Clientes pdf y excel

--- JS/PagesJS/Practica_Listado_Clientes.js
+++ JS/PagesJS/Practica_Listado_Clientes.js
@@ -4,7 +4,7 @@ console.log(`Funcionando Archivo:  Practica_Listado_Clientes`);
 function exportToExcel() {
 
     try {
-        let table = ContenedorTabla; // Asegúrate de que tu tabla tenga este ID
+        let table = document.getElementById('ttabla'); // Asegúrate de que tu tabla tenga este ID
         let wb = XLSX.utils.table_to_book(table);
         XLSX.writeFile(wb, "empleados.xlsx");
     } catch (error) {
