2dc010d47e03999158c5a67057ecebb5aaa38673
vlammp <mmorap@vlacademy.com>
2025-02-10 21:13:47 -0600

    Entega 4 proyecto Consumu de APi 2

--- JS/PagesJS/API_Listado_Clientes.js
+++ JS/PagesJS/API_Listado_Clientes.js
@@ -2,18 +2,21 @@ console.log('Funcionando Archivo: API_Listado_Clientes');
 
 const Api_Usuarios = "https://jsonplaceholder.typicode.com/users"
 
+const Api2 = "https://rickandmortyapi.com/api/character"
 
 // Datos locales simulando la API
 
 
 
 let DatosFiltrados = []
+let DatosFiltrados2 = []
 let contenedor = document.getElementById('tbody');
 
 
 //aela Visualizar al ejecutar la pagina DOM
 document.addEventListener('DOMContentLoaded', () => {
     consultarDatos() //Cuando trabajamos con APIS se llama a la funcion de consultar 
+    consultarDatos2()
     
 });
 
@@ -38,6 +41,24 @@ function consultarDatos() {
     
 }
 
+function consultarDatos2() {
+
+    //fetchget
+    fetch(Api2) //1- se le indica la url de la api 
+        
+        .then(response => response.json()) // 2 -Conversion de datos , JSON 
+        .then(result => {           //3- Resultado  result
+            // custom error
+            console.log(result.results)
+        
+        })
+        .catch(error => {      ///4- Catch
+            // common error
+            alert("Error ",error)
+            return null;
+        });
+    
+}
 
 
 
